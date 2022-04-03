4c4fac40060371f014771cb3be0e6424f254de21
Dakoog <taktoja@spoko.pl>
2022-04-03 12:05:25 +0200

    Add task fifth

---
+++ src/JavaBasic/DaySecond/TaskFifth.java
@@ -0,0 +1,31 @@
+package JavaBasic.DaySecond;
+
+import JavaBasic.DaySecond.Task1.StringHelper;
+
+/*Napisz program, któryprzyjmie jako argument input (typu String) i znak (typu char), a następnie
+zwróciliczbęwystąpieńpodanego znaku w danym łańcuchuznaków.
+Przykład:
+Input: Ala ma kota
+Searched Character: a
+Result: */
+public class TaskFifth {
+    public static void main(String[] args) {
+      String input = "Ala ma kota.";
+      char letter = 'z';
+        System.out.println(numberChars(input,letter));
+    }
+
+    private static int numberChars(String input, char searchedCharacter) {
+        int counter =0;
+                                         // jeśli danej litery nie ma 
+       if(StringHelper.isValid(input) && input.contains(String.valueOf(searchedCharacter))){
+           for (int i = 0; i <input.length() ; i++) {
+               if(input.charAt(i)== searchedCharacter){
+                   counter++;
+               }
+
+           }
+       }
+        return counter;
+    }
+}
