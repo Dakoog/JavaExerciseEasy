ccf1b4cb075cdb1daec2cb95dccecdfe41cf3be2
Dakoog <taktoja@spoko.pl>
2022-04-03 09:00:50 +0200

    New taskes

--- src/JavaBasic/DayOne/Task1/TaskFirst.java
+++ src/JavaBasic/DayOne/Task1/TaskFirst.java
@@ -1,4 +1,4 @@
-package JavaBasic.DayOne;
+package JavaBasic.DayOne.Task1;
 
 public class TaskFirst {
     /*
---
+++ src/JavaBasic/DayOne/TaskSeventh.java
@@ -0,0 +1,87 @@
+package JavaBasic.DayOne;
+
+import com.sun.jdi.FloatValue;
+
+import java.util.Scanner;
+    /*
+    Napisz program realizujący prosty kalkulator Program powinien
+    a) pobrać pierwszą liczbę (typu float
+    b) pobrać jeden ze znaków
+    c)pobrać drugą liczbę (typu float
+    d) zwrócić wynik pobranego działania
+    * Jeśli użytkownik poda znak inny niż obsługiwane, program powinien wypisać Operator not exist i zakończyć program
+    * Jeśli wpisanego działania nie da się zrealizować (tj jest niezgodne z zasadami matematyki), to
+    program powinien wypisać napis Wrong operation
+    * * *
+    Dodaj funkcjonalność, która pozwoli na wielokrotne wykonywanie działań, a z akończenie działania programu nastąpi po wpisaniu tekstu */
+
+public class TaskSeventh {
+    private static final String STOP_DECISION = "Stop";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String decision;
+        do {
+            System.out.print("Please insert firstDigit: ");
+            float firstDigit = scanner.nextFloat();
+            scanner.nextLine();
+
+            System.out.print("""
+                    Please insert operator
+                    + - add
+                    - - difference
+                    * - product
+                    : - quotient
+                    pwr - power
+                    """);
+            String operator = scanner.nextLine();
+
+            System.out.print("Please insert secondDigit: ");
+            float secondDigit = scanner.nextFloat();
+            scanner.nextLine();
+
+            float result = calculate(firstDigit, secondDigit, operator);
+            System.out.println("Result: " + result);
+
+            System.out.print("New calculation or Stop? ");
+            decision = scanner.nextLine();
+        } while (!STOP_DECISION.equals(decision));
+    }
+
+
+    private static float calculate(float firstDigit, float secondDigit, String operator) {
+        switch (operator) {
+            case "+" -> {
+                System.out.println(firstDigit + " + " + secondDigit);
+                return firstDigit + secondDigit;
+            }
+            case "-" -> {
+                System.out.println(firstDigit + " - " + secondDigit);
+                return firstDigit - secondDigit;
+            }
+            case "*" -> {
+                System.out.println(firstDigit + " * " + secondDigit);
+                return firstDigit * secondDigit;
+            }
+            case "/" -> {
+                System.out.println(firstDigit + " : " + secondDigit);
+
+                if (secondDigit == 0) {
+                    System.out.println("Wrong operation");
+                } else
+
+                    return firstDigit / secondDigit;
+            }
+            case "pwr" -> {
+                System.out.println(firstDigit + " ^ " + secondDigit);
+                if (secondDigit <= 0) {
+                    System.out.println("Wrong operation");
+                }
+                return (float) Math.pow(firstDigit, secondDigit);
+            }
+            default -> System.out.println("Operator not exist");
+        }
+        return Float.parseFloat(operator);
+    }
+}
