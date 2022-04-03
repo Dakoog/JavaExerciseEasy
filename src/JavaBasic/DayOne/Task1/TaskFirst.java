7de12cfe1f2184d61d533776618e815e4dc31fd4
Dakoog <taktoja@spoko.pl>
2022-04-03 22:27:26 +0200

    Comentary on the  first task of the first day

--- src/JavaBasic/DayOne/Task1/TaskFirst.java
+++ src/JavaBasic/DayOne/Task1/TaskFirst.java
@@ -5,25 +5,34 @@ public class TaskFirst {
         Napisz program, który wyświetli na ekranie wyraz SDA, nie używając ani jednego znaku " lub '
          */
     public static void main(String[] args) {
-        displayWorldUsingDecimalFormat();
+
+        // nie używając apostrofu ani tez cudzysłowia
+        // -> czyli bez string czy, też char
+        // -> przejście na Decimal, zapis binarny lub Hexagonal
+        displayWorldUsingDecimalFormat();// zapis dziesiątkowy
         System.out.println();
-        displayWorldUsingBinaryFormat();
+        displayWorldUsingBinaryFormat();// zapis binarny
         System.out.println();
-        displayWorldUsingBinaryHex();
+        displayWorldUsingBinaryHex();// zapis dziesiątkowy
 
     }
 
     private static void displayWorldUsingDecimalFormat() {
-        char s = 83;
-        char d = 68;
-        char a = 65;
-        System.out.print(s);
-        System.out.print(d);
-        System.out.print(a);
+        // zapis dziesiętny znaków graficznych -> liter
+        // duże litery są od 65 do 97
+        char S = 83;
+        char D = 68;
+        char A = 65;
+        // wypisujemy każdy char osobno w jednej linii -> print!
+        System.out.print(S);
+        System.out.print(D);
+        System.out.print(A);
 
     }
 
     private static void displayWorldUsingBinaryFormat() {
+        // zapis dwójkowy -> [ 0 1 ] -> zerojedynkowy -> binarny
+        // dopisujemy przed zapisem 0b
         char S = 0b01010011;
         char D = 0b01000100;
         char A = 0b01000001;
@@ -33,6 +42,7 @@ public class TaskFirst {
 
     }
     private static void displayWorldUsingBinaryHex() {
+        // zapis szesnastkowy (hexagonal) -> dopisujemy prefix 0x przed zapisem
         char S = 0x53;
         char D = 0x44;
         char A = 0x41;
