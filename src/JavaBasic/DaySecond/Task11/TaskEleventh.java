deaefa374a01d64254b2b3c7ac071742444e87a8
Dakoog <taktoja@spoko.pl>
2022-04-03 14:31:29 +0200

    Add task 13 and modifity task 11

--- src/JavaBasic/DaySecond/Task11/TaskEleventh.java
+++ src/JavaBasic/DaySecond/Task11/TaskEleventh.java
@@ -1,14 +1,27 @@
 package JavaBasic.DaySecond.Task11;
+/*
+    Napisz program, który będzie sprawdzał czy podane słowo (typu String) jest palindromem.
+    Przykład:
+    Input: kajak
+    Result: true
+    Input: sda
+    Result: false
+*/
+import JavaBasic.DaySecond.Helper.StringHelper;
 
 public class TaskEleventh {
     public static void main(String[] args) {
+
         String input = "kajak";
         System.out.println(isPalindrome(input));
         input = "programowanie";
         System.out.println(isPalindrome(input));
     }
-    private static boolean isPalindrome(String input){
-       return new StringBuilder(input).reverse().toString().equals(input);
 
+    private static boolean isPalindrome(String input) {
+        if (StringHelper.isValid(input)) {
+
+    }return new StringBuilder(input).reverse().toString().equals(input);
     }
+
 }
