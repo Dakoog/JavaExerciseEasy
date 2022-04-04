aca3c5bae8c4b4bc4c35fdb9ecc00fa7f28758c1
Dakoog <taktoja@spoko.pl>
2022-04-05 00:32:20 +0200

    modify taskes third and fourth

--- src/JavaBasic/DayOne/Task3/TaskThird.java
+++ src/JavaBasic/DayOne/Task3/TaskThird.java
@@ -7,7 +7,7 @@ public class TaskThird {
     private static final Scanner INPUT = new Scanner(System.in);
 
     /*
-            Zapisz program, który pobierze od użytkownika dwie zmienne x i y (typu int).
+            Napisz program, który pobierze od użytkownika dwie zmienne x i y (typu int).
             Następnie, za pomocą operatorów logicznych i matematycznych wyświetli wynik następujących zapytań:
             a) Czy x jest większe od y?
             b) Czy x pomnożone przez 3 jest większe od y?
@@ -15,15 +15,17 @@ public class TaskThird {
             d) Czy iloczyn liczb x i y jest parzysty
             */
     public static void main(String[] args) {
+        // wczytanie z metody getValue zmiennych x i y (przy użyciu Scannera zadeklarowane jako pole finalne INPUT)
         int x = getValueX();
         int y = getValueY();
-        System.out.println(isXUpperThanY(x,y));
-        System.out.println(isTripleXHiggerThanY(x,y));
-        System.out.println(isPre_incrementationXOrPost_incrementationY(x,y));
-        System.out.println(isProductXAndYEven(x,y));
+        // metody sprawdzające:
+        System.out.println(isXUpperThanY(x,y));// czy x jest większy niż igrek
+        System.out.println(isTripleXHiggerThanY(x,y));// potrojony x jest większy niż igrek
+        System.out.println(isPre_incrementationXOrPost_incrementationY(x,y));// inkremetnacje post i pre w zmiennych x oraz y
+        System.out.println(isProductXAndYEven(x,y));// czy iloczyn jest parzysty
 
     }
-
+    // metody pobierające dane od użytkownika
     private static int getValueX() {
 
         System.out.println("Please insert value x = ");
@@ -35,7 +37,7 @@ public class TaskThird {
         System.out.println("Please insert value y= ");
         return INPUT.nextInt();
     }
-
+  // metody sprawdzające (wpisywanie warunku w return)
     private static boolean isXUpperThanY(int x, int y) {
         return x > y;
     }
@@ -45,7 +47,7 @@ public class TaskThird {
     }
 
     private static boolean isPre_incrementationXOrPost_incrementationY(int x, int y) {
-        return y++ < ++x && --x < y++;
+        return (y++ < ++x) && (--x < y++);
 
     }
 
--- src/JavaBasic/DayOne/Task4/TaskFourth.java
+++ src/JavaBasic/DayOne/Task4/TaskFourth.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
        Napisz program obliczający BMI (Body Mass Index) oraz sprawdzający, czy jest ono w normie.
    Program powinien wczytać od użytkownika dwie zmienne: wagę (weight) w kilogramach (typ
    float) oraz wzrost (height) w centymetrach (typ int). BMI powinno zostaćwyliczone zgodnie
-   z poniższymwzorem:
+   z poniższym wzorem:
    Optymalny zakres BMI wynosi od 18,5 do 24,9, wartości mniejsze lub większe są wartościami
    nieoptymalnymi. Twój program powinien wypisać „BMI is correct” lub „BMI is not correct”,
-   zgodnie z powyższymizałożeniami.
+   zgodnie z powyższymi założeniami.
    Przykład:
    Please insert weight in kg: 80
    Please insert height in cm: 185
@@ -16,44 +16,47 @@ import java.util.Scanner;
     */
 
 public class TaskFourth {
-    private static final Scanner input = new Scanner(System.in);
+    // określenie finalnymi polami zakresu prawidłowej wagi w skali BMI
     private static final double BMI_MIN_RANGE = 18.5;
-    public static final double BMI_MAX_RANGE = 24.9;
+    private static final double BMI_MAX_RANGE = 24.9;
+    // przypisanie do pola finalnego Scannera
+    private static final Scanner INPUT = new Scanner(System.in);
+
 
     // static -> będzie to obiekt wspólny dla wszystkich wywołań
 
     public static void main(String[] args) {
-
         int height = getHeightFromUser();
         float weight = getWeightFromUser();
         float bmi = calculateBMI(weight, height);
         displayBmiResult(bmi);
     }
 
+    // metody pobierające wagę i wzrost od użytkownika
     private static float getWeightFromUser() {
-
         System.out.println("Please insert weight in kg: ");
-        return input.nextFloat();
+        return INPUT.nextFloat();
 
     }
 
     private static int getHeightFromUser() {
-
         System.out.println("Please insert height in cm: ");
-        return input.nextInt();
+        return INPUT.nextInt();
+    }
 
-    }
-    private static float calculateBMI(final float weight, final int height){
-        // weight/height(m)^2;
+    private static float calculateBMI(final float weight, final int height) {
+        // change from units of the indicated height in centimeters to height in meters
         float heightInMeter = height / 100f;
-        return weight/(float)Math.pow(heightInMeter,2);
+        // formula for calculating BMI
+        return weight / (float) Math.pow(heightInMeter, 2);
     }
-    private static void displayBmiResult(final float bmi){
-        if(bmi< BMI_MIN_RANGE || bmi > BMI_MAX_RANGE){
-            System.out.println("BMI is not correct "+ bmi);
-        }
-        else {
-            System.out.println("BMI is correct: "+ bmi);
+
+    private static void displayBmiResult(final float bmi) {
+        // wprowadzenie pól finalnych czytelnych do odbioru (nie nic niemówiące wartości)
+        if (bmi < BMI_MIN_RANGE || bmi > BMI_MAX_RANGE) {
+            System.out.println("BMI is incorrect " + bmi);
+        } else {
+            System.out.println("BMI is correct: " + bmi);
         }
     }
 }
