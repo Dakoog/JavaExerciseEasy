b4840ec9c598e38f079552525692d3bfd5508adb
Dakoog <taktoja@spoko.pl>
2022-04-05 14:07:45 +0200

    Modify task tenth with first day

--- src/JavaBasic/DayOne/Task10/TaskTenth.java
+++ src/JavaBasic/DayOne/Task10/TaskTenth.java
@@ -9,25 +9,46 @@ package JavaBasic.DayOne.Task10;
     Sum Of Digits: 6
 */
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TaskTenth {
     public static void main(String[] args) {
-        int number = getNumber();
+
+        int number;
+        do {
+            // if number is double or float
+            try {
+                number = getNumber();
+            } catch (InputMismatchException ime) {
+                number = -1;
+            }
+            // if number is negative value
+            if (number < 0) {
+                System.out.println("This isn't a natural number");
+            }
+
+        } while (number < 0);// use when number is negative value
+
+          // calculate sum of digits given number
         int DigitsSum = sumOfDigitWithNumber(number);
         System.out.println("The sum of the digits of the number " + number + " is " + DigitsSum);
 
     }
 
+
+
+// download number for user
     private static int getNumber() {
-        System.out.println("Please insert number: ");
+        System.out.println("Please insert natural number: ");
         Scanner input = new Scanner(System.in);
         return input.nextInt();
     }
-
+// calculate calculate sum of digits given number
     private static int sumOfDigitWithNumber(int number) {
         int sum = 0;
         int digit;
+
         do {
             digit = number % 10;
             sum += digit;
