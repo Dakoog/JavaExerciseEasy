eeec820eff58c343e4b966e39536ea0c0ec19679
Dakoog <taktoja@spoko.pl>
2022-04-09 00:01:38 +0200

    Add task seventh of SecondDay

--- src/JavaBasic/DaySecond/Task7/TaskSeventh.java
+++ src/JavaBasic/DaySecond/Task7/TaskSeventh.java
@@ -1,17 +1,29 @@
 package JavaBasic.DaySecond.Task7;
 
+import JavaBasic.NewMethod.RoundsNumbers;
+
 import java.util.Scanner;
     /*
-    Napisz program realizujący prosty kalkulator Program powinien
-    a) pobrać pierwszą liczbę (typu float
-    b) pobrać jeden ze znaków
-    c)pobrać drugą liczbę (typu float
-    d) zwrócić wynik pobranego działania
-    * Jeśli użytkownik poda znak inny niż obsługiwane, program powinien wypisać Operator not exist i zakończyć program
-    * Jeśli wpisanego działania nie da się zrealizować (tj jest niezgodne z zasadami matematyki), to
-    program powinien wypisać napis Wrong operation
-    * * *
-    Dodaj funkcjonalność, która pozwoli na wielokrotne wykonywanie działań, a z akończenie działania programu nastąpi po wpisaniu tekstu */
+    Napisz program, który pobierze od użytkownika tekst (typu String)
+     i policzy jakim procentem wszystkich znaków tekstu był znak spacji*/
+
 
 public class TaskSeventh {
+    public static void main(String[] args) {
+        System.out.println("The percentage of spaces in the entire text is "+percentageSpacesInText()+"%.");
+    }
+    private static String getTextforUser(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please insert your text");
+
+        return input.nextLine();
+
+    }
+
+    private static double percentageSpacesInText(){
+        String text = getTextforUser();
+        String onlyLetters = text.replaceAll(" ", "");
+        double spaces = text.length()-onlyLetters.length();
+        return RoundsNumbers.hundredths(spaces/text.length()*100d);
+    }
     }
