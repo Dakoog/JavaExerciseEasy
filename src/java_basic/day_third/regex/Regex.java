aea1a7e1eaeffea3520d32b8f92be2d83bae51fa
Dakoog <taktoja@spoko.pl>
2022-04-10 10:11:24 +0200

    Add package regex with class Regex

---
+++ src/java_basic/day_third/regex/Regex.java
@@ -0,0 +1,20 @@
+package java_basic.day_third.regex;
+
+public class Regex {
+    // Regex -> wyrażenie regularne.
+    /*  ||         ZNAKI       ||  KWANTYFIKATORY    ||              GRUPOWANIE                         ||
+        ___________________________________________________________________________________________________
+         \d – dowolna liczba   ||  * – 0 lub więcej  || [ ] – dopasowuje wszystkie znaki w nawiasach    ||
+         \D – nie liczba       ||  + – 1 lub więcej  || [^ ] – dopasowuje wszystkie znaki spoza nawiasów||
+         \w – dowolna litera   ||  ? – 0 lub 1       || () – grupowanie                                 ||
+         \W – nie litera       ||  {}– dok. licz. zn.|| | – albo                                        ||
+         \s – biały znak       ||  {min,max}– zakres ||
+         \S – nie biały znak   ||
+         . – dowolny znak      ||
+         \ – wyjściowy znak    ||
+         \b – granica słowa    ||
+         \B – nie granica słowa||
+         ^ – początek ciągu    ||
+         $ – koniec ciągu      ||
+*/
+}
