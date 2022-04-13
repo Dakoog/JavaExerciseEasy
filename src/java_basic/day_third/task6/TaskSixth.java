a53e4a1e70b7f1d7a0d97f6a1276c2cad03005a6
Dakoog <taktoja@spoko.pl>
2022-04-13 23:31:56 +0200

    Modify TaskSixth ->  change format date and print

--- src/java_basic/day_third/task6/TaskSixth.java
+++ src/java_basic/day_third/task6/TaskSixth.java
@@ -24,22 +24,22 @@ public class TaskSixth {
     }
     private static void numberOfDaysToMeeting(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Please insert date your next SDA meeting in format yyyy-MM-dd HH:mm:ss");
+        System.out.println("Please insert date for your SDA meeting.");
         String meetingDate = input.nextLine();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime nextLessons = LocalDateTime.parse(meetingDate,formatter);
+        LocalDateTime nextLessons = LocalDateTime.parse(meetingDate, formatter);
 
         LocalDateTime now = LocalDateTime.now();
 
         Duration duration = Duration.between(now, nextLessons);
-        int year = (int)(duration.toDaysPart()/-365.24);
-        int days = -(int)(duration.toDaysPart() +year*365.24);
-        long hours = -duration.toHoursPart();
-        int minutes = -duration.toMinutesPart();
-        int secs = -duration.toSecondsPart();
+        long days = duration.toDaysPart();
+        int hours = duration.toHoursPart();
+        int minutes = duration.toMinutesPart();
+        int secs = duration.toSecondsPart();
         System.out.println("You have Days: " + days + " Hours: " + hours + " Minutes: " + minutes + " Seconds: " + secs);
-        System.out.printf("You have : %02d Days: %02d Hours: %02d Minutes: %02d Seconds: %02d",year, days, hours, minutes, secs);
+        System.out.printf("You have Days: %02d Hours: %02d Minutes: %02d Seconds: %02d", days, hours, minutes, secs);
         System.out.printf("\n%02d:%02d:%02d:%02d", days, hours, minutes, secs);
+
 
     }
 }
