2e8f8a452fbbb5e6571ec08d955f5433bca59d43
Dakoog <taktoja@spoko.pl>
2022-04-20 18:51:41 +0200

    Modify taskSixth with package day_third/task6

--- src/java_basic/day_third/task6/TaskSixth.java
+++ src/java_basic/day_third/task6/TaskSixth.java
@@ -27,18 +27,19 @@ public class TaskSixth {
         System.out.println("Please insert date for your SDA meeting.");
         String meetingDate = input.nextLine();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime nextLessons = LocalDateTime.parse(meetingDate, formatter);
-
-        LocalDateTime now = LocalDateTime.now();
-
-        Duration duration = Duration.between(now, nextLessons);
-        long days = duration.toDaysPart();
-        int hours = duration.toHoursPart();
-        int minutes = duration.toMinutesPart();
-        int secs = duration.toSecondsPart();
-        System.out.println("You have Days: " + days + " Hours: " + hours + " Minutes: " + minutes + " Seconds: " + secs);
-        System.out.printf("You have Days: %02d Hours: %02d Minutes: %02d Seconds: %02d", days, hours, minutes, secs);
-        System.out.printf("\n%02d:%02d:%02d:%02d", days, hours, minutes, secs);
+        System.out.printf("%1$td %1$tB %1$tY, %1$tA %n",meetingDate);
+//        LocalDateTime nextLessons = LocalDateTime.parse(meetingDate, formatter);
+//
+//        LocalDateTime now = LocalDateTime.now();
+//
+//        Duration duration = Duration.between(now, nextLessons);
+//        long days = duration.toDaysPart();
+//        int hours = duration.toHoursPart();
+//        int minutes = duration.toMinutesPart();
+//        int secs = duration.toSecondsPart();
+//        System.out.println("You have Days: " + days + " Hours: " + hours + " Minutes: " + minutes + " Seconds: " + secs);
+//        System.out.printf("You have Days: %02d Hours: %02d Minutes: %02d Seconds: %02d", days, hours, minutes, secs);
+//        System.out.printf("\n%02d:%02d:%02d:%02d", days, hours, minutes, secs);
 
 
     }
