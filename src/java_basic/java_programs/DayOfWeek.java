0f7a1bec2b25feaf6aaf4bb7ab891d0aa631d5ce
Dakoog <taktoja@spoko.pl>
2022-04-20 18:54:32 +0200

    Add new tasks DayOfWeek and YourAge

--- src/java_basic/java_programs/DayOfWeek.java
+++ src/java_basic/java_programs/DayOfWeek.java
@@ -1,2 +1,15 @@
-package java_basic.java_programs;public class DayOfWeek {
+package java_basic.java_programs;
+
+public class DayOfWeek {
+
+    private static final String START_DATE = "2000-12-31";//sunday
+
+    public static void main(String[] args) {
+
+    }
+    private static String howDayOfWeekWasThisDate(){
+        String day = "";
+
+        return day;
+    }
 }
